995d34f5c9aabba11eff947522ac86a8d8e3043b
czar <cyobero@gmail.com>
2019-11-12 18:29:22 -0800

    modified views

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from posts.models import Posts
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 def home(request):
-    return render(request, 'index.html')
+    # Fetcch posts andn only display first five posts.
+    # We'll use pagination to scroll through posts.
+    post_list = Posts.objects.all()
+    paginator = Paginator(post_list, 5)  # paginator object that contains latest five posts
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # retrieves first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range then deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'home.html', {'posts': posts})
+
 
 def about(request):
     return render(request, 'about.html')
