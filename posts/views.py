35155b122a9a18b564ae5ef10d49e76685007ee9
czar <cyobero@gmail.com>
2019-11-12 17:05:20 -0800

    created view for post model

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.http import Http404
+from posts.models import Posts
 
 from django.shortcuts import render
 
 # Create your views here.
+def post(request, post_id):
+    # make sure that post_id is an int
+    try:
+        post_id = int(post_id)
+    except ValueError:
+        raise Http404
+
+    post = Posts.objects.get(id=post_id)
+
+    return render(request, 'post.html', {
+        'title': post.title,
+        'caption': post.caption,
+        'author': post.author,
+        'body': post.body,
+        'timestamp': post.timestamp,
+    })
