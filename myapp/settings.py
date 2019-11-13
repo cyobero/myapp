3ba71f07264cd4689b46a9a68ad71a6cc6569e26
czar <cyobero@gmail.com>
2019-11-12 16:03:49 -0800

    modified settings and index.html

--- myapp/settings.py
+++ myapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-016x+a=$1#8f$w)ex0g!626c!)@!=0x5##tv9-_0=gu!+=nge'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['turing-poet-258403.appspot.com', 'localhost']
+ALLOWED_HOSTS = ['turing-poet-258403.appspot.com', 'localhost', 'http://127.0.0.1:8000/']
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
 ]
 
 MIDDLEWARE = [
